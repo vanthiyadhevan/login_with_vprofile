ba1fc318d2b76f832387db09449f6904c22a0990
vanthiyadhevan <samvickey96@gmail.com>
2024-01-09 12:34:55 +0530

    First Commit

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Apply deployment
+kubectl apply -f deployment.yaml
+
+# Apply service
+kubectl apply -f service.yaml
+
+# Wait for the deployment to be ready
+kubectl rollout status deployment vprofile-deployment
